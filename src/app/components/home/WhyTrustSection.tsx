d4040939447011f06bbf13f9b316857f9ba71a15
liljdot <jehuoki@gmail.com>
2025-03-21 19:46:14 +0100

    feat: add WhyTrustSection component to showcase achievements

---
+++ src/app/components/home/WhyTrustSection.tsx
@@ -0,0 +1,33 @@
+const Achievment: React.FC<{ value: string, achievment: string }> = ({value, achievment}) => {
+
+    return (
+        <div className="card bg-transparent">
+            <div className="card-body text-center flex flex-col gap-5">
+                <h2 className="text-7xl font-bold">{value}+</h2>
+                <p className="text-lg">{achievment}</p>
+            </div>
+        </div>
+    )
+}
+
+const WhyTrustSection: React.FC = () => {
+
+    return (
+        <>
+            <section className="w-full flex flex-col justify-center bg-accent py-25">
+                <h3 className="text-5xl text-center font-bold px-75">Why Small Medium Enterprises Trust Altbuilda.</h3>
+
+                <div className="grid grid-cols-3 px-37.5 mt-23 gap-y-39">
+                    <Achievment value="10,000" achievment="Businesses Served"/>
+                    <Achievment value="50" achievment="SME Industries Supported"/>
+                    <Achievment value="$100,000" achievment="In Total Transactions Processed"/>
+                    <Achievment value="20,000" achievment="Total Registered Users"/>
+                    <Achievment value="5,000" achievment="Invoices Generated"/>
+                    <Achievment value="$10,000" achievment="Total Credit Processed"/>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default WhyTrustSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import ToggleTheme from "./components/ToggleTheme";
 import HeroSection from "./components/home/HeroSection";
+import WhyTrustSection from "./components/home/WhyTrustSection";
 
 export default function Home() {
   return (
     <>
       <main className="p-0 bg-neutral">
         <HeroSection />
+        <WhyTrustSection />
         <ToggleTheme />
       </main>
     </>
