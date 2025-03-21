eb6c76bfb9fbac4c6d1e8e49382b8850d7652577
liljdot <jehuoki@gmail.com>
2025-03-21 13:39:43 +0100

    refactor: update ActionButton component to use children prop instead of content

--- src/app/components/ActionButton.tsx
+++ src/app/components/ActionButton.tsx
@@ -3,17 +3,17 @@ import { IoIosArrowRoundForward } from "react-icons/io"
 
 interface Props {
     to: string
-    content: string
+    children?: any
     filled?: boolean
     text?: "primary"
 }
 
-const ActionButton: React.FC<Props> = ({to, content, filled, text}) => {
+const ActionButton: React.FC<Props> = ({to, children, filled, text}) => {
 
     return (
         <>
             <Link href={to} className={`btn ${filled ? "bg-primary text-secondary" : `bg-transparent ${text ? "text-primary" : "text-white"}`} border border-primary w-60 rounded-full text-xl font-medium p-6.5 shadow-none hover:scale-105 transition-all ease-in-out duration-500 hover:[&>*]:translate-x-2`}>
-                {content}
+                {children}
                 <IoIosArrowRoundForward className={`${filled ? "bg-primary text-secondary" : `${text ? "text-primary" : "text-white"}`} size-8 transition-all ease-in-out duration-500`} />
             </Link>
         </>
